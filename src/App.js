91e759101d43dcf385b05345e2fd48caa3905ccd
Chrispolanco <chrispolanco@sbcglobal.net>
2021-12-28 15:31:16 -0800

    completed mini application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import './App.css'; 
+import Clock from "./Clock";
+import {Form, FormControl, Button } from 'react-bootstrap'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    
+    constructor(props){
+        super(props); 
+        this.state = {
+            deadline: 'December 25, 2025', 
+            newDeadLine: ''
+        }
+    }
+
+    changeDeadline(){
+        this.setState({deadline: this.state.newDeadLine}); 
+    }
+
+    render(){
+        return(
+            <div className='App'>
+                <div className= "App-title">
+                    Countdown to {this.state.deadline}
+                </div>
+                <Clock
+                    deadline = {this.state.deadline}
+                /> 
+                <Form inline>
+                    <FormControl 
+                    className="Deadline-input"
+                    placeholder='New Date' 
+                    onChange={event => this.setState({newDeadLine: event.target.value}) }
+                    /> 
+                    <Button onClick={() => this.changeDeadline()}>Submit</Button>
+                </Form>
+            </div>
+        )
+    }
+
 }
 
-export default App;
+export default App; 
